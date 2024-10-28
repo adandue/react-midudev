40d9ba1c09dcdc1e0554fdcd3380c26c635744dc
adan_due <ing.adanduenas@gmail.com>
2024-10-28 11:51:00 -0600

    Diferencia de sintaxis entre js y jsx

---
+++ button.jsx
@@ -0,0 +1,8 @@
+import React from 'https://esm.sh/react@18.2.0'
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0/client'
+
+<React.Fragment>
+    <button data-id='123'>Button 1</button>
+    <button data-id='456'>Button 2</button>
+    <button data-id='789'>Button 3</button>
+</React.Fragment>
