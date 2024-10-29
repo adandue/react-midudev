6087c52ab6714cabea7a643f10707a5f42976519
adan_due <ing.adanduenas@gmail.com>
2024-10-29 12:45:27 -0600

    Pasando booleanos como props

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -4,8 +4,10 @@ import { TwitterFollowCard } from './Components/TwitterFollowCard'
 const App = () => {
     return (
         <section className='App'>
-            <TwitterFollowCard userName='Adan_Duenas' name='Adán Dueñas Escobar' />
-            <TwitterFollowCard userName='veroo_monteroo' name='monTeroo!' />
+            <TwitterFollowCard isFollowing userName='adandue' name='Adán Dueñas Escobar' />
+            <TwitterFollowCard isFollowing={false} userName='veroo_monteroo' name='monTeroo!' />
+            <TwitterFollowCard isFollowing userName='midudev' name='Miguel Ángel Durán' />
+            <TwitterFollowCard isFollowing={false} userName='freddier' name='Freddy Vega' />
         </section>
     )
 }
