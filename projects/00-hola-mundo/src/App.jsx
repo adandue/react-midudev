bb0341870e9bd3d6fb44abf0ec39d5c8f5be3834
adan_due <ing.adanduenas@gmail.com>
2024-11-04 11:52:55 -0600

    Las props como estados iniciales, solo se inicializan una vez

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -3,19 +3,22 @@ import './App.css'
 import { TwitterFollowCard } from './Components/TwitterFollowCard'
 
 const App = () => {
+    const [isFollowing, setIsFollowing] = useState(false)
+    const changeState = () => {
+        setIsFollowing(!isFollowing)
+    }
+
+    console.log('[App] render with isFollowing: ', isFollowing)
 
     return (
         <section className='App'>
-            <TwitterFollowCard userName='adandue' initialIsFollowing={true}>
+            <TwitterFollowCard userName='adandue' initialIsFollowing={isFollowing}>
                 Adán Dueñas Escobar
             </TwitterFollowCard>
-            {/*Así se pueden escribir comentarios en jsx */}
-            <TwitterFollowCard userName='veroo_monteroo' >
-                monTeroo!
-            </TwitterFollowCard>
-            <TwitterFollowCard  userName='midudev' >
-                Miguel Ángel Durán
-            </TwitterFollowCard>
+
+            <button onClick={changeState}>
+                Cambiar estado de App
+            </button>
         </section>
 
     )
