b33fb516a94e0c7d87340284190cc0ee65320c0a
adan_due <ing.adanduenas@gmail.com>
2024-11-14 11:59:34 -0600

    Actualizando el tablero al hacer click

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -2,22 +2,31 @@ import { useState } from "react"
 
 
 const TURNS = {
-  x: 'x',
+  X: 'x',
   O: 'o'
 }
 
 
 
-const Square = ({children, updateBoard, index}) => {
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected': ''}`
+  const handleClick = () => {
+    updateBoard()
+  }
   return (
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
-  const [board, setBoard] = useState(['x','x','x','o','o','o','x','o','x'])
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () => {
+
+  }
 
   return (
     <>
@@ -30,12 +39,21 @@ function App() {
                 <Square  
                   key={index}
                   index={index}
+                  updateBoard={updateBoard}
                 >
                   {board[index]}
                 </Square>
               )
             })
           }
+        </section>
+        <section className="turn">
+          <Square isSelected={turn === TURNS.X}>
+            {TURNS.X}
+          </Square>
+          <Square isSelected={turn === TURNS.O}>
+            {TURNS.O}
+          </Square>
         </section>
       </main>
     </>
