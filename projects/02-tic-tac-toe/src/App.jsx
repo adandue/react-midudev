530c7c7512996a2df1408f8aeed4df88b454f73f
adan_due <ing.adanduenas@gmail.com>
2024-11-19 11:39:29 -0600

    Detectar al ganador

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -20,23 +20,57 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1 , 2],
+  [3, 4 , 5],
+  [6, 7 , 8],
+  [0, 3 , 6],
+  [1, 4 , 7],
+  [2, 5 , 8],
+  [0, 4 , 8],
+  [2, 4 , 6],
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) //null: no hay ganador, false: hay un empate
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras
+    //para ver si X u O  ganó
+    for (const combo of WINNER_COMBOS) {
+      const [a,b,c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a] //x u o
+      }
+    }
+    //si no hay ganador
+    return null
+  }
 
   const updateBoard = (index) => {
     //No actualizamos esta posición si ya tiene algo
-    if(board[index]) return
+    if(board[index] || winner) return
     //actualizar el tablero
     const newBoard = [...board]
     //spread y rest operators son muy importantes
     //porque los estados siempre se deben de tratar como inmutables
     //los datos modificados siempre deben ser nuevos
     newBoard[index] = turn
-    setBoard(newBoard)
+    setBoard(newBoard) //asíncrona
     //cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    //revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)// la actualización de los estados en React es asíncrona
+      alert(`El ganador es ${newWinner}`)
+    }
   }
 
   return (
